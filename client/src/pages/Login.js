f7d0502287dec0cb8b7ba027d1c4bb46be7afac9
Brian Ramirez <brians@uninorte.edu.co>
2018-11-14 10:35:11 -0500

    [feat]: login view updated

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,10 +5,33 @@ import GoogleLogin from 'react-google-login';
 import Button from '@material-ui/core/Button';
 import { Redirect } from 'react-router-dom';
 import HttpClient from '../services/HttpClient';
+import logo from '../images/rr-services-white-logo.png'
+import bg from '../images/bg.jpg'
 
 
 const styles = theme => ({
-
+  root: {
+    width: '100%',
+    height: '100vh',
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    backgroundImage: `url(${bg})`,
+    backgroundRepeat:'no-repeat',
+    backgroundSize:'cover',
+    backgroundPosition:'center'
+  },
+  loginButton:{
+    marginBottom: '70px',
+    color: 'white',
+    borderColor: 'white !important',
+    width: '120px'
+  },
+  logo:{
+    height: "200px"
+  }
 });
 
 class Login extends React.Component {
@@ -30,18 +53,20 @@ class Login extends React.Component {
   };
   
   render() {
-    // const { classes } = this.props;
+    const { classes } = this.props;
     return ( 
-      this.state.redirect ? <Redirect to="/" /> : (<div>
-        <GoogleLogin
-          clientId="695297879400-o8hcj1k3dnpjo9mm2todbdbr65kudn3n.apps.googleusercontent.com"
-          scope="email"
-          render={renderProps => (
-            <Button variant="contained" color="primary" onClick={renderProps.onClick}>Login</Button>
-          )}
-          onSuccess={this.responseGoogle}
-          onFailure={this.responseGoogle}
-        />
+      this.state.redirect ? <Redirect to="/" /> : (<div className={classes.root}>
+      <img className={classes.logo} src={logo}></img>
+      <GoogleLogin
+        clientId="695297879400-o8hcj1k3dnpjo9mm2todbdbr65kudn3n.apps.googleusercontent.com"
+        scope="email"
+        render={renderProps => (
+          <Button variant="outlined" className={classes.loginButton} color="primary" onClick={renderProps.onClick}>Login</Button>
+        )}
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+      />
+      
     </div>)
     );
   }
